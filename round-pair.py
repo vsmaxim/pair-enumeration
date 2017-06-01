421796b16d7b54f030523d4e18397358ec30d264
vsmaxim8 <vsmaxim8@gmail.com>
2017-06-01 23:42:56 +0300

    Добавлен интерфейс для перестановки пар через элемент

--- round-pair.py
+++ round-pair.py
@@ -1,12 +1,23 @@
 from itertools import combinations
 
-def permutatePairs(a, i): 
+def permutateAllPairs(a, i): 
 	for j in range(0, len(a), 2):
 		a[(j + i) % len(a)], a[(j + i + 1) % len(a)] = a[(j + i + 1) % len(a)], a[(j + i) % len(a)]
 	return a
 
+def permutateSkip(a, i):
+    i %= 3
+    if (i == 2):
+        for i in range(0, len(a), 3):
+            a[i % len(a)], a[(i + 1) % len(a)] = a[(i + 1) % len(a)], a[i % len(a)]
+    else:
+        for i in range(i, len(a), 3):
+            a[(i - 1) % len(a)], a[(i - 2) % len(a)] = a[(i - 2) % len(a)], a[(i - 1) % len(a)]
+    return a
+
+
 def rTalkPairs(a):
-	return [tuple(sorted(a[i:i+2])) for i in range(len(a))]
+	return [tuple(sorted(a[i:i+2])) for i in range(0, len(a), 2)]
 
 def lTalkPairs(a):
 	res = []
@@ -22,6 +33,13 @@ print(permutatePairs(currentPlacement, num))
 print(lTalkPairs(currentPlacement))
 needToTalk = [x for x in needToTalk if (x not in lTalkPairs(currentPlacement))]
 needToTalk = [x for x in needToTalk if x not in rTalkPairs(currentPlacement)]
-currentPlacement = permutatePairs(currentPlacement, 0)
-needToTalk = [x for x in needToTalk if x not in lTalkPairs(currentPlacement)]
+print(needToTalk)
+i = 1
+while needToTalk:
+    i = 1 - i
+    currentPlacement = permutatePairs(currentPlacement, i)
+    input()
+    print(rTalkPairs(currentPlacement))
+    print(currentPlacement)
+    needToTalk = [x for x in needToTalk if x not in rTalkPairs(currentPlacement)]
 print(needToTalk)
