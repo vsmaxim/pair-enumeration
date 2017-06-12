07c9ab3cd8478fda18d1403597fc90351959d01f
vsmaxim8 <vsmaxim8@gmail.com>
2017-06-12 15:49:48 +0300

    Added gitignore, added readme.md

--- pair.py
+++ pair.py
@@ -1,10 +1,9 @@
 from itertools import combinations, permutations
 
-_elementsNum = 6
+_elementsNum = int(input('Input elements num: ')) 
 _defaultElements = tuple(int(i) for i in range(1, _elementsNum + 1))
 
 pairs = list(combinations(_defaultElements, 2))
-print(pairs)
 groups = list(combinations(pairs, _elementsNum // 2))
 checkedGroups = []
 for group in groups: 
@@ -13,9 +12,7 @@ for group in groups:
 		b += list(i)
 	if len(b) == len(set(b)):
 		checkedGroups.append(group)
-print(checkedGroups)
 result = []
-print(len(list(combinations(checkedGroups, _elementsNum - 1))))
 for plan in combinations(checkedGroups, _elementsNum - 1):
 	b = []
 	for i in plan:
--- round-pair.py
+++ round-pair.py
@@ -1,11 +1,14 @@
 from itertools import combinations
 
 def permutateAllPairs(a, i): 
-	for j in range(0, len(a), 2):
-		a[(j + i) % len(a)], a[(j + i + 1) % len(a)] = a[(j + i + 1) % len(a)], a[(j + i) % len(a)]
-	return a
+    ''' Пересадка всех людей с соседями, если i четное, то с правым, если i
+    нечетное, то с левым'''
+    for j in range(0, len(a), 2):
+        a[(j + i) % len(a)], a[(j + i + 1) % len(a)] = a[(j + i + 1) % len(a)], a[(j + i) % len(a)]
+    return a
 
 def permutateSkip(a, i):
+    ''' Пересадка с пропуском в одного человека, a - массив, i - номер человека'''
     i %= 3
     if (i == 2):
         for i in range(0, len(a), 3):
@@ -17,19 +20,25 @@ def permutateSkip(a, i):
 
 
 def rTalkPairs(a):
-	return [tuple(sorted(a[i:i+2])) for i in range(0, len(a), 2)]
+    ''' Разговор пар в правую сторону'''
+    return [tuple(sorted(a[i:i+2])) for i in range(0, len(a), 2)]
 
 def lTalkPairs(a):
-	res = []
-	for i in range(0, len(a), 2):
-		res.append(tuple(sorted([a[i], a[i - 1]])))
-	return res
-pairNum = 6
+    ''' Разговор пар в левую сторону'''
+    res = []
+    for i in range(0, len(a), 2):
+        res.append(tuple(sorted([a[i], a[i - 1]])))
+    return res
 
+
+# Пример работы с парами за круглым столом
+pairNum = 6
 currentPlacement = [i for i in range(1 , pairNum + 1)]
+currentPlacement = permutateSkip(currentPlacement, 2)
+print(currentPlacement)
 needToTalk = list(combinations(currentPlacement, 2))
 num = int(input("Input pair permutation: "))
-print(permutatePairs(currentPlacement, num))
+print(permutateAllPairs(currentPlacement, num))
 print(lTalkPairs(currentPlacement))
 needToTalk = [x for x in needToTalk if (x not in lTalkPairs(currentPlacement))]
 needToTalk = [x for x in needToTalk if x not in rTalkPairs(currentPlacement)]
@@ -37,7 +46,7 @@ print(needToTalk)
 i = 1
 while needToTalk:
     i = 1 - i
-    currentPlacement = permutatePairs(currentPlacement, i)
+    currentPlacement = permutateAllPairs(currentPlacement, i)
     input()
     print(rTalkPairs(currentPlacement))
     print(currentPlacement)
